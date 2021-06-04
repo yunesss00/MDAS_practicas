e906ac77b19fbe8df3e1630ad0d13f145f9bf44a
Youness Bettahi Berdai <48675571+yunesss00@users.noreply.github.com>
2021-06-04 02:04:13 +0200

    funcionalidad dar de alta abono completa
    sin probar

--- Implementación/src/es/uco/mdas/datos/Abono.java
+++ Implementación/src/es/uco/mdas/datos/Abono.java
@@ -1,26 +1,54 @@
 package es.uco.mdas.datos;
 
 import java.util.Date;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
 
 public class Abono {
 	
-	private Long idAbono;
-	private Date fechaExpedicionAbono;
+	private Long idAbono = (long) 0;
+	private LocalDate fechaExpedicionAbono;
 	private String deporteAbono;
 	private String tipoAbono;
+	private int localidad = 0;
+
 	
-	public Abono(Long idAbono, Date fechaExpedicionAbono, String deporteAbono, String tipoAbono) {
-		this.idAbono = idAbono;
-		this.fechaExpedicionAbono = fechaExpedicionAbono;
+	public Abono() {}
+	
+	public Abono(String deporteAbono, String tipoAbono) {
 		this.deporteAbono = deporteAbono;
 		this.tipoAbono = tipoAbono;
+		this.localidad = localidad++;
+		
+		//Calcular la fecha de expedicion para la siguiente temporada
+		String mesDia= "-09-16";
+		LocalDate fechaExpedicionAbono = null;
+		Date date = new Date();
+		int añoExpedicion;
+		String aux;
+		
+        ZoneId zonaHoraria = ZoneId.systemDefault();
+        LocalDate fechaActual = date.toInstant().atZone(zonaHoraria).toLocalDate();
+        añoExpedicion = fechaActual.getYear() + 1;
+        aux = Integer.toString(añoExpedicion) + mesDia;
+        
+        fechaExpedicionAbono = LocalDate.parse(aux);
+		this.fechaExpedicionAbono = fechaExpedicionAbono;
+		
+		//Calcular la id del abono
+		int idAbonoMax = 9999;
+		Long idAleatorio = (long) Math.random()*idAbonoMax;
+		this.idAbono = idAleatorio;	
+	
 	}
+	
 	
 	public Long getIdAbono() {
 		return idAbono;
 	}
 	
-	public Date getFechaExpedicionAbono() {
+	public LocalDate getFechaExpedicionAbono() {
 		return fechaExpedicionAbono;
 	}
 	
@@ -32,15 +60,24 @@ public class Abono {
 		return tipoAbono;
 	}
 	
+	public int getLocalidad() {
+		return localidad;
+	}
+	
+	
 	public void setIdAbono(Long idAbono) {
 		this.idAbono = idAbono;
 	}
 	
-	public void setFechaExpedicionAbono(Date fechaExpedicionAbono) {
-		this.fechaExpedicionAbono = fechaExpedicionAbono;
+	public void setFechaExpedicionAbono(LocalDate fechaExpedicionRenovada) {
+		this.fechaExpedicionAbono = fechaExpedicionRenovada;
 	}
 	
 	public void setDeporteAbono(String deporteAbono) {
 		this.deporteAbono = deporteAbono;
+	}
+	
+	public void setLocalidad(int localidad) {
+		this.localidad = localidad;
 	}
 }
--- Implementación/src/es/uco/mdas/datos/ComponenteAbonos.java
+++ Implementación/src/es/uco/mdas/datos/ComponenteAbonos.java
@@ -28,11 +28,52 @@ public class ComponenteAbonos extends Abono{
 	}
 	
 	public static ComponenteAbonos getInstance() {
-        if(instance == null) {
+        if(instance == null) 
+        {
             instance = new ComponenteAbonos();
         }
         return instance;
     }
+	
+	/**
+	 * Si no quedan abonos return false, si quedan return true
+	 * @param deporte
+	 * @return
+	 */
+	public Boolean comprobarDisponibilidadAbono(String deporte) {
+		File f;
+		String nombreFichero = nombreFicheroAbonosDisponibles; 
+		f= new File(nombreFichero);
+		
+		try {
+			
+			if (!f.exists()) 
+			{
+				System.out.println("No existe el fichero que busca.");
+				return false;
+			}
+			
+			BufferedReader reader = new BufferedReader(new FileReader(nombreFichero));
+			String linea;
+			System.out.println("Comprobando numero de abonos disponibles");
+			String[] partes;
+			
+			while((linea = reader.readLine()) != null)
+			{
+				
+				partes = linea.split("-");
+				linea = partes[1];
+			
+				if(partes[0] == deporte && linea.trim().equals(Integer.toString(0)))
+					return false;
+			}
+			reader.close();
+		
+		}catch(IOException e) {}
+		return true;
+	}
+	
+	
 	/**
 	 * Es correcto si el deporte y el tipo de abono existen en el sistema
 	 * @param abono
@@ -45,12 +86,55 @@ public class ComponenteAbonos extends Abono{
 		return false;
 
 	}
+	
+	
+	/**
+	 * Si encuentra la id del socio return true, si no retun false,
+	 * si no encuentra el archivo return false
+	 * @param abono
+	 * @param idSocio
+	 * @return
+	 */
+	public Boolean comprobarExistenciaAbono(Abono abono, Long idSocio) {
+		File f;
+		String nombreFichero = "abonos" + abono.getDeporteAbono() + ".txt"; 
+		f= new File(nombreFichero);
+		
+		try {
+			
+			if (!f.exists()) 
+			{
+				System.out.println("No existe el fichero que busca.");
+				return false;
+			}
+			
+			BufferedReader reader = new BufferedReader(new FileReader(nombreFichero));
+			String linea;
+			String[] partes;
+			System.out.println("Comprobando si el socio ya posee este abono");
+			
+			while((linea = reader.readLine()) != null) 
+			{
+				partes = linea.split("-");
+				linea = partes[1];
+				if(linea.trim().equals(Long.toString(idSocio))) {
+					return true;
+				}
+			}
+			reader.close();
+			
+		}catch(IOException e) {}
+		return false;
+	}
+	
 	/**
 	 * Da de alta un abono de un tipo y deporte determinados.
 	 * @param abono
 	 * @param idSocio
 	 */
 	public void darDeAltaAbono(Abono abono, Long idSocio) {
+		
+		comprobarDisponibilidadAbono(abono.getDeporteAbono());
 		
 		File f;
 		String nombreFichero = "abonos" + abono.getDeporteAbono() + ".txt"; 
@@ -62,35 +146,133 @@ public class ComponenteAbonos extends Abono{
 			{	
 				f.createNewFile();
 			}
+
+			FileWriter w = new FileWriter(f.getAbsoluteFile(),true);
+			BufferedWriter bw = new BufferedWriter(w);
+			PrintWriter wr = new PrintWriter(bw);
 			
-			BufferedReader reader = new BufferedReader(new FileReader(nombreFichero));
-			String linea;
-			
-			while((linea = reader.readLine()) != null) 
+			if (comprobarDatosAbono(abono) && !comprobarExistenciaAbono(abono,idSocio)) 
 			{
-				if (linea .contains(Long.toString(idSocio)))
-				{
-					System.out.println("El socio ya dispone de este abono.");
-					break;
-				}
-				
-				FileWriter w = new FileWriter(f.getAbsoluteFile(),true);
-				BufferedWriter bw = new BufferedWriter(w);
-				PrintWriter wr = new PrintWriter(bw);
-				
-				if (comprobarDatosAbono(abono)) 
-				{
-					wr.append(abono.getIdAbono() + "_" + idSocio + "\n");
-				}
-				else 
-				{
-					System.out.println("El deporte o el tipo de abono son incorrectos.");
-				}
-				
-				wr.close();
-				bw.close();
+				wr.append(abono.getIdAbono() + "-" + idSocio + "\n");
 			}
+			else 
+			{
+				System.out.println("El deporte o el tipo de abono son incorrectos.");
+			}
+			
+			wr.close();
+			bw.close();
+			generarAbono(abono,idSocio);
+			modificarAbonosDisponibles(abono.getDeporteAbono());
 		
 		}catch (IOException e) {}
 	}
+	
+	/**
+	 * Genera un fichero con los datos del abono creado
+	 * @param abono
+	 * @param idSocio
+	 */
+	public void generarAbono(Abono abono, Long idSocio) {
+		
+		File f;
+		String nombreFichero = Long.toString(idSocio) + ".txt";
+		f= new File(nombreFichero);
+		
+		try {
+			
+			if(!f.exists()) 
+			{	
+				f.createNewFile();
+			}
+			
+			FileWriter w = new FileWriter(f.getAbsoluteFile(),true);
+			BufferedWriter bw = new BufferedWriter(w);
+			PrintWriter wr = new PrintWriter(bw);
+			
+			wr.append(abono.getIdAbono() + "\n"); 
+			wr.append(abono.getDeporteAbono() + "\n");
+			wr.append(abono.getTipoAbono() + "\n");
+			wr.append(abono.getFechaExpedicionAbono() + "\n");
+			wr.append(abono.getLocalidad() + "\n");
+			
+			wr.close();
+			bw.close();
+
+		} catch(IOException e) {}
+	}
+	
+	/**
+	 * Resta en uno los abonos disponibles de un deporte en concreto
+	 * @param deporte
+	 */
+	public void modificarAbonosDisponibles(String deporte) {
+		File ficheroLectura;
+		File ficheroEscritura;
+		ficheroLectura = new File(nombreFicheroAbonosDisponibles);
+		ficheroEscritura = new File("temporal.txt");
+		
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(ficheroLectura));
+			BufferedWriter writer = new BufferedWriter(new FileWriter(ficheroEscritura));
+
+			String linea;
+			String abonosDisponibles;
+			int nuevosAbonosDisponibles;
+			String[] partes;
+			while((linea = reader.readLine()) != null) 
+			{
+				if(linea.contains(deporte)) 
+				{
+					partes = linea.split("-");
+					abonosDisponibles = partes[1];
+					nuevosAbonosDisponibles = Integer.parseInt(abonosDisponibles) - 1;
+					linea = deporte + "-" + nuevosAbonosDisponibles;
+					writer.write(linea + "\n");
+				}
+			}
+			reader.close();
+			writer.close();
+			ficheroLectura.delete();
+			boolean correcto = ficheroEscritura.renameTo(ficheroLectura);
+			if(correcto)
+			{
+				System.out.println("Se ha renomrado satisfactoriamente el fichero.");
+			}
+			else
+			{
+				System.out.println("No se ha renombrado correctamente.");
+			}
+			
+		}catch(IOException e) {}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
