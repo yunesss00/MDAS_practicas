54d3190ffb70e2ab0da848bf8750c870fc6024be
Cristian1711 <i82goorc@uco.es>
2021-06-06 02:36:25 +0200

    arreglado?

--- Implementación/src/es/uco/mdas/sistema/socio/RegistrarSocioImp.java
+++ Implementación/src/es/uco/mdas/sistema/socio/RegistrarSocioImp.java
@@ -2,13 +2,15 @@ package es.uco.mdas.sistema.socio;
 
 import es.uco.mdas.negocio.socio.ObjetoSocio;
 import es.uco.mdas.negocio.socio.SocioMgt;
+import es.uco.mdas.negocio.socio.SocioMgtImp;
+import es.uco.mdas.negocio.socio.datos.AbonoDatosImp;
+import es.uco.mdas.negocio.socio.datos.SocioDatosImp;
 
-public class RegistrarSocioImp implements RegistrarSocio{
-	
+public class RegistrarSocioImp implements RegistrarSocio{	
 	private SocioMgt gestorSocio;
-	
+
 	public RegistrarSocioImp() {
-		this.gestorSocio = new ClubDeportivoMgtImp(new RegistarSocioImp());
+		this.gestorSocio = new SocioMgtImp(new AbonoDatosImp(), new SocioDatosImp());
 	}
 
 	@Override
